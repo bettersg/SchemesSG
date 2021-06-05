49eeefe536ef283903c2250943152a2729f6e0a9
W T <tanw.89@gmail.com>
2021-06-05 19:54:39 +0800

    added loading text

--- static/js/schemescase.js
+++ static/js/schemescase.js
@@ -1,8 +1,8 @@
 const scriptURLschemescase = 'https://script.google.com/macros/s/AKfycbyqGHt2p224ebUahB6XDOgxtru9fvXm3YonCKcPus--p8e57TFB/exec'
-const schemescase = document.getElementById("schemescase")
+const schemescase = document.getElementById("schemescase1")
 schemescase.addEventListener('submit', e => {
     e.preventDefault()
-    $("#schemescase-submit").html("Submitting...");
+    $("#schemescase-submit1").html("Submitting...");
     fetch(scriptURLschemescase, { method: 'POST', body: new FormData(schemescase) })
         .then(response => window.location.replace("gotit.html"))
         .catch(error => console.error('Error!', error.message))
