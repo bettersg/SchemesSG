ff0e43ab9057d91f8c6ce48082f434ce8147382f
W T <tanw.89@gmail.com>
2021-06-05 17:50:23 +0800

    removed loading text

--- static/js/schemescase.js
+++ static/js/schemescase.js
@@ -2,7 +2,6 @@ const scriptURLschemescase = 'https://script.google.com/macros/s/AKfycbyqGHt2p22
 const schemescase = document.getElementById("schemescase")
 schemescase.addEventListener('submit', e => {
     e.preventDefault()
-    $("#schemescase-submit").html("Submitting...");
     fetch(scriptURLschemescase, { method: 'POST', body: new FormData(schemescase) })
         .then(response => window.location.replace("gotit.html"))
         .catch(error => console.error('Error!', error.message))
