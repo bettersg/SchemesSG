78dd13f464f136d181b5c621e0cf2fdf61084465
qlim6 <62872180+qlim6@users.noreply.github.com>
2022-02-04 19:15:17 +0800

    Update query.py

--- new_model_quintus/query.py
+++ new_model_quintus/query.py
@@ -4,6 +4,11 @@ import numpy as np
 import pandas as pd
 from sentence_transformers import SentenceTransformer, util
 from spellchecker import SpellChecker
+# Remove if not using LSI
+import nltk
+import string
+import re
+from gensim import corpora, models, similarities
 
 df = pd.read_csv('df.csv')
 
@@ -11,6 +16,8 @@ model = SentenceTransformer('paraphrase-mpnet-base-v2')
 
 with open('emb.pkl', 'rb') as handle:
     emb = pickle.load(handle)
+with open('mh_emb.pkl', 'rb') as handle:
+    mh_emb = pickle.load(handle)
 
 spell = SpellChecker()
 # Add words to vocabulary. Can continue adding as new queries come in, but would advise not to go too far; may inadvertently generate a lot of false positives
@@ -54,24 +61,63 @@ def autocorrect(text):
     
 def scale(x):
     '''Takes (arrays of) numbers between -1 to 1, scales to be between 0 to 100'''
-    return (x + 1) / 2 * 100
+    return (x + 1) * 50
 
-def search_similar_schemes(text, relevance = 0, n = 5, spellcheck = True):
+def call_lsi(search):
+    '''Calls the LSI model & returns the row index of the top result in df.csv'''
+    with open('dic.pkl', 'rb') as handle:
+        dic = pickle.load(handle)
+    with open('corpus.pkl', 'rb') as handle:
+        corpus = pickle.load(handle)
+    lsi = models.LsiModel(corpus, id2word = dic, num_topics = 300)
+    tokens = nltk.word_tokenize(search.strip())
+
+    # Get top 3 indexes for LSI
+    vec_bow = dic.doc2bow(tokens)
+    vec_lsi = lsi[vec_bow]
+    index = similarities.MatrixSimilarity(lsi[corpus])
+    return np.argmax(index[vec_lsi])
+
+def search_similar_schemes(text, relevance = 0, n = 5, spellcheck = True, lsi = True):
     '''Takes a user search, returns top n results. "relevance" is the relevance score threshold (from the old API)'''
     
+    # Clean user search
     text = str(text).strip()
-    
     search = text
     if spellcheck:
         temp = autocorrect(text)
         if isinstance(temp, tuple):
             search, _ = temp
     
+    # Get vanilla similarity to schemes
     query = model.encode(search)
     sim = util.pytorch_cos_sim(query, emb)
     sim = np.array(sim[0])
+    
+    # Add a weight for mental health queries
+    mh_sim = util.pytorch_cos_sim(query, mh_emb)
+    mh_sim = np.array(mh_sim[0]).max()
+    
+    mh = [
+        'abuse', 'bullying', 'bullied', 'depressed', 'depression', 'suicid', 'troubled', 'violen', ' stress', 'mental health', 'distress', 'scared', 'assault', 'trauma',
+        'counselling', 'counsellor', 'therapist', 'therapy', 'psychiatri', 'psycholog',
+        'autistic', 'autism', 'schizo', 'epilepsy', 'adhd', 'addict', 'dementia', 'alzheimer']
+    
+    mh_schemes = 'mental health|counselling|emotional care|casework'
+    
+    if (mh_sim > .7) | (any(substring in search.lower() for substring in mh)):
+        mh_index = df['Scheme Type'].str.lower().str.contains(mh_schemes) | df['What it gives'].str.lower().str.contains(mh_schemes)
+        # After scaling, relevance score will increase by 10
+        sim[mh_index] += .2
+    
     index = np.argsort(-sim)[:n]
     
+    # Add alternative results from LSI
+    if lsi:
+        lsi_index = call_lsi(search)
+        if (lsi_index not in index) & (scale(sim[lsi_index]) > .5):
+            index = np.append(index, lsi_index)
+        
     # Get relevance scores & filter df
     sim = scale(sim[index])
     df_out = df.loc[index, ['Scheme', 'Description', 'Agency', 'Image', 'Link']]
